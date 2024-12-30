71b8046436574885ed9ab61b2940c262e916455d
subbu20204 <subbu20204@gmail.com>
2024-12-30 19:02:42 +0530

    commit

--- Main/js/main.js
+++ Main/js/main.js
@@ -1,4 +1,5 @@
-const baseUrl = "http://localhost:3001/";
+// const baseUrl = "http://localhost:3000/";
+const baseUrl = "https://bis-server.vercel.app/";
 
 const handleAuth = (event) => {
   event.preventDefault();
